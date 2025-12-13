296fb9fd32611363d37f343e640267325cda404b
Jalal Barau <jalalbarau@gmail.com>
2025-12-13 11:09:34 +0100

    Fix Flask app export and unit test

--- test_app.py
+++ test_app.py
@@ -1,9 +1,7 @@
 from app import app
 
-def test_root_route():
+def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert response.data.decode() == "WRONG_TEXT_FOR_TEST"
-
-
+    assert response.data.decode("utf-8") == "Hello, World!"
